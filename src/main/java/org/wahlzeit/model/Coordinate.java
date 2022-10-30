4c9cc7ef50d70b48cfbb46a4c43efe5e321679db
DanyFabFau <daniel.fabrikantow@fau.de>
2022-10-30 21:45:35 +0100

    added 2 new methods (getDistance, isEqual) and overrides 'equals' for Coordinate

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -44,4 +44,35 @@ public class Coordinate {
 	public double getZ() {
 		return z;
 	}
+
+    /**
+     * Calculates the euclidian distance to a given coordinate
+     * @param coordinate
+     * @return euclidian distance
+     */
+    public double getDistance(Coordinate coordinate) {
+        double distX = coordinate.getX() - this.getX();
+        double distY = coordinate.getY() - this.getY();
+        double distZ = coordinate.getZ() - this.getZ();
+
+        return Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2) + Math.pow(distZ, 2));
+    }
+
+    /**
+     * Checks if this coordinate is equal to the given one
+     * @param coordinate
+     * @return true if all 3 values (x, y, z) are identical to the given coordinate
+     */
+    public boolean isEqual(Coordinate coordinate) {
+        return this.getX() == coordinate.getX() &&
+               this.getY() == coordinate.getY() &&
+               this.getZ() == coordinate.getZ();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // order can be important -> if it's not an instance of Coordinate, false is returned instantly
+        return object instanceof Coordinate &&
+               this.isEqual((Coordinate) object);
+    }
 }
